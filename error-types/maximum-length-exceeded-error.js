9ca21b004dcf5e07a92d2d385bcaed6931149b2f
Dvwhite81 <123031200+Dvwhite81@users.noreply.github.com>
2023-08-08 22:39:01 -0400

    Add files via upload

---
+++ error-types/maximum-length-exceeded-error.js
@@ -0,0 +1,23 @@
+const ValidationError = require('./validation-error');
+
+class MaximumLengthExceededError extends ValidationError {
+  constructor(excessLength, ...params) {
+    super(...params);
+
+    this.name = 'MaximumLengthExceededError';
+    this.message = 'Maximum length exceeded';
+    
+    if (excessLength) {
+      this.message = `Maximum length exceeded by ${excessLength}`;
+    }
+  }
+}
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = MaximumLengthExceededError;
+} catch {
+  module.exports = null;
+}
